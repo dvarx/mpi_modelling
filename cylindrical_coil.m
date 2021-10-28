578a36b445673a4f873e3df14be5a4c4b59f0dea
dvarx <dvarx@ethz.ch>
2021-10-28 22:26:36 +0200

    added third coil and adjusted geometry to match system geometry of TNB MNS

--- cylindrical_coil.m
+++ cylindrical_coil.m
@@ -1,36 +1,52 @@
 % compute the field of a cylindrical solenoid
 
 mu0=4*pi*1e-7;
+Ncoil=1000;
+di=50e-3;
+do=90e-3;
+J=10/((90e-3-50e-3)*85e-3);
 
 %coil structure describing the coil
 %coil 1 definition
-coil_1.do=30e-2;
-coil_1.di=20e-2;
-coil_1.h=30e-2;
+coil_1.do=90e-3;
+coil_1.di=50e-3;
+coil_1.h=85e-3;
 coil_1.i=1;
-coil_1.ox=20e-2;
-coil_1.oy=20e-2;
+coil_1.ox=-49e-3;
+coil_1.oy=28.29e-3;
+coil_1.J=J;
 %coil 2 definition
-coil_2.do=30e-2;
-coil_2.di=20e-2;
-coil_2.h=30e-2;
+coil_2.do=90e-3;
+coil_2.di=50e-3;
+coil_2.h=85e-3;
 coil_2.i=1;
-coil_2.ox=-20e-2;
-coil_2.oy=-20e-2;
+coil_2.ox=49e-3;
+coil_2.oy=28.29e-3;
+coil_2.J=J;
+%coil 3 definition
+coil_3.do=90e-3;
+coil_3.di=50e-3;
+coil_3.h=85e-3;
+coil_3.i=1;
+coil_3.ox=0;
+coil_3.oy=-56.58e-3;
+coil_3.J=J;
 
-coil_config.coils=[coil_1,coil_2];
+coil_config.coils=[coil_1,coil_2,coil_3];
 
 %current density
 N=25;
-xs=linspace(-1,1,N);
-ys=linspace(-1,1,N);
-zs=linspace(-1,1,N);
+xs=linspace(-0.2,0.2,N);
+ys=linspace(-0.2,0.2,N);
+zs=linspace(-0.3,0.3,N);
 
 coil_config.zs_plot=zeros(N*N*N,1);
 coil_config.xs_plot=zeros(N*N*N,1);
 coil_config.ys_plot=zeros(N*N*N,1);
 
 coil_1.fields=zeros(N*N*N,3);
+coil_2.fields=zeros(N*N*N,3);
+coil_3.fields=zeros(N*N*N,3);
 
 % fields(i,j) field at position zs(i), rs(j)
 tic
@@ -56,6 +72,10 @@ for i=1:1:N
             coil_2.fields(linidx,:)=coil_field(sqrt(x^2+y^2),z,phi,coil_2);
             coil_2.fields_cartesian(linidx,3)=coil_2.fields(linidx,3);
             coil_2.fields_cartesian(linidx,1:2)=coil_2.fields(linidx,1:2)';
+            %compute field of coil 3
+            coil_3.fields(linidx,:)=coil_field(sqrt(x^2+y^2),z,phi,coil_3);
+            coil_3.fields_cartesian(linidx,3)=coil_3.fields(linidx,3);
+            coil_3.fields_cartesian(linidx,1:2)=coil_3.fields(linidx,1:2)';
         end
     end
 end
@@ -63,7 +83,13 @@ toc
 
 coil_config.coils(1).fields_cartesian=coil_1.fields_cartesian;
 coil_config.coils(2).fields_cartesian=coil_2.fields_cartesian;
+coil_config.coils(3).fields_cartesian=coil_3.fields_cartesian;
 
 plot_coil_field(coil_config,1);
+hold on;
+quiver3(zeros(3,1),zeros(3,1),zeros(3,1),[0.25;0;0],[0;0.25;0],[0;0;0.25],'color','black');
 plot_coil(coil_1,1);
 plot_coil(coil_2,1);
+plot_coil(coil_3,1);
+
+axis equal;
